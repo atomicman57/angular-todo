cb018648d439f6dfc55aac10760f86bf84f8a314
Blessing Philips <blessingadesina@gmail.com>
2017-10-15 15:15:31 +0100

    Project Setup

---
+++ src/app/todo-data.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { TodoDataService } from './todo-data.service';
+
+describe('TodoDataService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TodoDataService]
+    });
+  });
+
+  it('should be created', inject([TodoDataService], (service: TodoDataService) => {
+    expect(service).toBeTruthy();
+  }));
+  
+  describe('#getAllTodos()', () => {
+    
+  })
+});
